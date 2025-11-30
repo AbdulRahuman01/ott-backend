e0ff9079f62a117d8507334fd24c9841247c1955
Abdul <abdulrahuman6281@gmail.com>
2025-12-01 01:46:37 +0530

    Switch production DB to SQLite

--- ott_platform/settings.py
+++ ott_platform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,16 +87,28 @@ WSGI_APPLICATION = 'ott_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ott_platform',       # your database name
-        'USER': 'root',         # your MySQL username
-        'PASSWORD': '',  # your MySQL password
-        'HOST': '127.0.0.1',    # or '127.0.0.1'
-        'PORT': '3306',         # default MySQL port
+IS_RENDER = "RENDER" in os.environ
+
+if IS_RENDER:
+    # Production → SQLite
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    # Local development → MySQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ott_platform',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
+    }
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -167,7 +180,7 @@ ALLOWED_HOSTS = [
 ]
 
 
-import os
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
