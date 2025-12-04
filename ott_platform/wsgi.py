ea5e9b82c869e1f5a0e06965bd914c5edf548573
Abdul <abdulrahuman6281@gmail.com>
2025-12-04 18:38:23 +0530

    Add auto superuser creation

--- ott_platform/wsgi.py
+++ ott_platform/wsgi.py
@@ -14,3 +14,17 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ott_platform.settings')
 
 application = get_wsgi_application()
+
+from django.contrib.auth import get_user_model
+
+if os.environ.get("RENDER") == "true":
+    User = get_user_model()
+    admin_email = "admin@gmail.com"
+    admin_password = "Admin@123"
+
+    if not User.objects.filter(email=admin_email).exists():
+        User.objects.create_superuser(
+            email=admin_email,
+            password=admin_password,
+            name="Admin"
+        )
