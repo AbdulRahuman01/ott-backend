d9502ca693df1f3116f727011bb265511eeb72e5
Abdul <abdulrahuman6281@gmail.com>
2025-12-15 14:32:58 +0530

    custom user

--- users/models.py
+++ users/models.py
@@ -1,35 +1,45 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager ,PermissionsMixin
 from django.db import models 
 from django.conf import settings
 from movies.models import Movie
 from datetime import timedelta
 from django.utils import timezone
 
-class UserManager(BaseUserManager): 
-      def create_user(self, email, password=None): 
-            if not email: 
-                 raise ValueError("Users must have an email address") 
-            email = self.normalize_email(email) 
-            user = self.model(email=email) 
-            user.set_password(password) 
-            user.save(using=self._db) 
-            return user 
- 
-      def create_superuser(self, email, password): 
-        user = self.create_user(email, password) 
-        user.is_admin = True 
-        User.is_superuser = True 
-        user.save(using=self._db) 
-        return user 
- 
-class User(AbstractBaseUser): 
-    email = models.EmailField(unique=True) 
-    name = models.CharField(max_length =255) 
-    is_active = models.BooleanField(default=True) 
-    is_admin = models.BooleanField(default=False) 
-    objects = UserManager() 
- 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
 
 
 class Watchlist(models.Model):
