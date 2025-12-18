28e94bb71890dfa2ea33c6944489bebc70f96566
Abdul <abdulrahuman6281@gmail.com>
2025-12-18 12:47:59 +0530

    cors error checked

--- ott_platform/settings.py
+++ ott_platform/settings.py
@@ -174,18 +174,13 @@ AUTH_USER_MODEL = 'users.User'
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-     "https://ottmovieflix.netlify.app",
      "https://ottmovieflix.vercel.app",
        
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
     "https://ott-backend-original.onrender.com",
-    "https://ottmovieflix.netlify.app", 
     "https://ottmovieflix.vercel.app",
-    "https://*.vercel.app",
 ]
 
 
