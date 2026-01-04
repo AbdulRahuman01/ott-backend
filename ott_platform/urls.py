2ad3ccff2cf40490725292e99155d22bb4d0f051
Abdul <abdulrahuman6281@gmail.com>
2026-01-05 01:09:07 +0530

    Add temporary admin creation endpoint

--- ott_platform/urls.py
+++ ott_platform/urls.py
@@ -3,12 +3,31 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+import os
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
 
 from users.models import User
 from django.http import HttpResponse
 
 
+def create_admin(request):
+    User = get_user_model()
 
+    # Safety: do not create admin twice
+    if User.objects.filter(is_superuser=True).exists():
+        return JsonResponse(
+            {"message": "Admin already exists"},
+            status=400
+        )
+
+    # Create admin user
+    User.objects.create_superuser(
+        email=os.getenv("ADMIN_EMAIL", "admin@ottflix.com"),
+        password=os.getenv("ADMIN_PASSWORD", "Admin@123"),
+    )
+
+    return JsonResponse({"message": "Admin created successfully"})
 
 def go_to_admin_login(request):
     return redirect('/admin-panel/login/')
@@ -25,6 +44,7 @@ urlpatterns = [
 
     
    # 👈 THIS MUST BE INSIDE THE LIST
+   path("create-admin/", create_admin),
 ]
 
 
